362e09af5b0b74cfdd0d20d1b8ce6145f49a612a
bleriot nogguia <nstevebleriot@yahoo.fr>
2021-01-01 13:21:21 +0100

    User Registration & Password Encryption

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,7 @@ import User from '../models/userModel.js'
 import generateToken from '../utils/generateToken.js'
 
 // @desc     Auth user & get token
-// @route    GET /api/users
+// @route    GET /api/users/login
 // @access   Public
 const authUser = asyncHandler(async (req, res) => {
     const {email, password} = req.body
@@ -21,6 +21,39 @@ const authUser = asyncHandler(async (req, res) => {
     }else{
         res.status(401)
         throw new Error('Invalid email or password')
+    }
+})
+
+// @desc     Register a new user
+// @route    GET /api/users
+// @access   Public
+const registerUser = asyncHandler(async (req, res) => {
+    const {name, email, password} = req.body
+    
+    const userExists = await User.findOne({ email })
+
+    if(userExists){
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if(user){
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    }else{
+        res.status(400)
+        throw new Error('Invalid data')
     }
 })
 
@@ -42,4 +75,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
     }
 })
 
-export { authUser, getUserProfile }
+export { authUser, registerUser, getUserProfile }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { authUser, getUserProfile } from '../controllers/userController.js'
+import { authUser, getUserProfile, registerUser } from '../controllers/userController.js'
 import { protect } from '../middleware/authMiddleware.js'
 const router = express.Router()
 
 router.post('/login', authUser)
+router.post('/', registerUser)
 router.route('/profile').get(protect, getUserProfile)
 
 export default router
