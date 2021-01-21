931dff46a28a8218172c01740f1136ba1fa34d2f
bleriot nogguia <nstevebleriot@yahoo.fr>
2021-01-21 12:39:51 +0100

    Authentication updated

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -32,7 +32,7 @@ userSchema.pre('save', async function (next) {
     if(!this.isModified('password')){
         next()
     }
-    const salt = bcrypt.genSalt(10)
+    const salt = await bcrypt.genSalt(10)
     this.password = await bcrypt.hash(this.password, salt)
 })
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,9 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import LoginScreen from './screens/LoginScreen'
 import CartScreen from './screens/CartScreen'
+import RegisterScreen from './screens/RegisterScreen'
 
 const App = () => {
   return (
@@ -13,9 +15,11 @@ const App = () => {
       <Header />
         <main className="py-3">
           <Container>
-            <Route path='/' component={HomeScreen} exact />
+            <Route path='/login' component={LoginScreen} />
+            <Route path='/register' component={RegisterScreen} />
             <Route path='/product/:id' component={ProductScreen} />
             <Route path='/cart/:id?' component={CartScreen} />
+            <Route path='/' component={HomeScreen} exact />
           </Container>
         </main>
       <Footer />
