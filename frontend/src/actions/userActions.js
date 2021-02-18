ff375f51eda75f950bf937a8fdf9e8e87cf0ec80
bleriotnoguia <nstevebleriot@yahoo.fr>
2021-02-18 15:47:30 +0100

    User Edit Screen & Get User Details

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -224,7 +224,7 @@ export const deleteUsers = (id) => async (dispatch, getState) => {
             },
         }
 
-        const { data } = await axios.delete(`/api/users/${id}`, config)
+         await axios.delete(`/api/users/${id}`, config)
 
         dispatch({
             type: USER_DELETE_SUCCESS,
