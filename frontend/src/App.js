f0cdbe371c0d6f48b02d6122fc4c19745b8805ec
bleriotnoguia <nstevebleriot@yahoo.fr>
2021-02-25 13:45:43 +0100

    Product Search

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -39,6 +39,7 @@ const App = () => {
                     <Route path='/admin/orderlist' component={OrderListScreen} />
                     <Route path='/product/:id' component={ProductScreen} />
                     <Route path='/cart/:id?' component={CartScreen} />
+                    <Route path='/search/:keyword' component={HomeScreen} />
                     <Route path='/' component={HomeScreen} exact />
                 </Container>
             </main>
