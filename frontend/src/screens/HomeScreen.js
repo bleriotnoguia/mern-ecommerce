b97969e56fcdfd79287258538c539c56920ce363
bleriotnoguia <nstevebleriot@yahoo.fr>
2021-02-26 12:04:22 +0100

    Custom Page Titles & Meta

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
+import {Link} from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import Product from '../components/Product'
 import { listProducts } from '../actions/productActions'
@@ -7,6 +8,7 @@ import Message from '../components/Message'
 import Loader from '../components/Loader'
 import Paginate from '../components/Paginate'
 import ProductCarousel from '../components/ProductCarousel'
+import Meta from '../components/Meta'
 
 const HomeScreen = ({ match }) => {
     const keyword = match.params.keyword
@@ -15,8 +17,8 @@ const HomeScreen = ({ match }) => {
 
     const dispatch = useDispatch()
 
-    const productList = useSelector(state => state.productList)
-    const { loading, error, products, page, pages} = productList
+    const productList = useSelector((state) => state.productList)
+    const { loading, error, products, page, pages } = productList
 
     useEffect(() => {
         dispatch(listProducts(keyword, pageNumber))
@@ -24,24 +26,35 @@ const HomeScreen = ({ match }) => {
 
     return (
         <>
-        {!keyword && <ProductCarousel />}
-           <h1>Latest Products</h1>
-           {loading ? (
-               <Loader />
-               ) : error ? (
-               <Message variant="danger">{error}</Message>
-           ) : (
-               <>
-                <Row>
-                    {products.map(product => (
-                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                            <Product product={product} />
-                        </Col>
-                    ))}
-                </Row>
-                <Paginate pages={pages} page={page} keyword={keyword ? keyword : ''} />
+            <Meta />
+            {!keyword ? (
+                <ProductCarousel />
+            ) : (
+                <Link to='/' className='btn btn-light'>
+                    Go Back
+                </Link>
+            )}
+            <h1>Latest Products</h1>
+            {loading ? (
+                <Loader />
+            ) : error ? (
+                <Message variant='danger'>{error}</Message>
+            ) : (
+                <>
+                    <Row>
+                        {products.map((product) => (
+                            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                                <Product product={product} />
+                            </Col>
+                        ))}
+                    </Row>
+                    <Paginate
+                        pages={pages}
+                        page={page}
+                        keyword={keyword ? keyword : ''}
+                    />
                 </>
-           )} 
+            )}
         </>
     )
 }
